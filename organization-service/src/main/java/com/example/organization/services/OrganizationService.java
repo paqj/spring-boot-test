a902309b81f61bb39ffcc96446307edf74d75743
paqj <changraven@gmail.com>
2018-10-03 22:50:37 +0900

    Organization-service with EurekaSvr

---
+++ organization-service/src/main/java/com/example/organization/services/OrganizationService.java
@@ -0,0 +1,33 @@
+package com.example.organization.services;
+
+import com.example.organization.model.Organization;
+import com.example.organization.repository.OrganizationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class OrganizationService {
+    @Autowired
+    private OrganizationRepository orgRepository;
+
+    public Organization getOrg(String organizationId) {
+        return orgRepository.findByOrganizationId(organizationId);
+    }
+
+    public void saveOrg(Organization org){
+        org.setId( UUID.randomUUID().toString());
+
+        orgRepository.save(org);
+
+    }
+
+    public void updateOrg(Organization org){
+        orgRepository.save(org);
+    }
+
+    public void deleteOrg(Organization org){
+        orgRepository.deleteById( org.getId());
+    }
+}
